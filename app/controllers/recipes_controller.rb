c06ff79fa8ac1f316816a5e56218388cb3787d9d
mizuguchitomoko <shuikouzhizi7@gmail.com>
2021-02-05 05:35:06 +0000

    recipe new/createアクション実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,38 @@
 class RecipesController < ApplicationController
   def top
   end
+
+  def index
+    @recipe = Recipe.new
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    #@recipe.user_id = current_user.id
+    @recipe.save!
+    #redirect_to recipe_path(@recipe)
+    render :"top"
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title, :recipe_body, :image)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_071215) do
+ActiveRecord::Schema.define(version: 2021_02_05_042625) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "recipe_title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_071215) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
