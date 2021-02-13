9bbff05b2f88d9473d75b33e3b4f7929caf92799
mizuguchitomoko <shuikouzhizi7@gmail.com>
2021-02-13 07:18:10 +0000

    views/follows/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.followings.all.page(params[:page]).per(6)
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.all.page(params[:page]).per(6)
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id
-  has_many :followed, through: :active_relationships, source: :follower
+  has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :followed
 
