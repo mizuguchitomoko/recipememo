094367ef6656f21fb063110e5f10c4c3d1f3918c
mizuguchitomoko <shuikouzhizi7@gmail.com>
2021-02-18 07:10:23 +0000

    category/new create

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ApplicationRecord
   default_scope->{order(created_at: :desc)}
   has_many :favorites
   has_many :recipe_comments, dependent: :destroy
+  has_many :recipe_category_relations
+  has_many :categories, through: :recipe_category_relations
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
